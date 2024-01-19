1b8f1bc10040d4e1d633c382aa2dc01ba80d8147
Vijay Singh <rockcodingrookie21@gmail.com>
2024-01-19 23:22:35 +0530

    Version_1

---
+++ Data Structure and Alogrithem/Arrays/Max_Subarray_3_kadanes_algo.java
@@ -0,0 +1,22 @@
+public class Max_Subarray_3_kadanes_algo {
+
+    public static void kadans(int numbers[]) {
+        int cs = 0;
+        int ms = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if (cs < 0) {
+                cs = 0;
+
+            }
+            ms = Math.max(cs, ms);
+
+        }
+        System.out.println("our Max subarray sum :" + ms);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kadans(numbers);
+    }
+}
